3ed78bca1f7a1a0735c5fbb9dbc74cd4bd080dc6
Ryan Hope <rmh3093@gmail.com>
2015-10-08 16:41:29 -0400

    changes

--- crisp.py
+++ crisp.py
@@ -6,158 +6,202 @@ import types
 import simpy
 import numpy as np
 
-class BrainstemOscillator(object):
-	def __init__(self, env, sp, mean=.250, states=11, start_state=0):
+class Timer(object):
+	__alias__ = "timer"
+	def __init__(self, env, labile, mean=.250, states=11, start_state=0):
 		self.env = env
-		self.sp = sp
-		self.mean = mean
-		self.states = states
-		self.start_state = start_state if start_state < self.states else self.states-1
+		self.labile = labile
+		self.setStates(states)
+		self.setMean(mean)
+		if start_state == -1:
+			self.start_state = np.random.randint(self.states)
+		elif start_state >= self.states:
+			self.start_state = self.states - 1
+		else:
+			self.start_state = start_state
 		self.process = env.process(self.run())
+
+	def setStates(self, states):
+		self.states = states
+		self.env.log(0, self.__alias__, "set_states", self.states)
+
+	def setMean(self, mean):
+		self.mean = mean
+		self.env.log(0, self.__alias__, "set_mean", self.mean)
 
 	def next_state(self):
 		yield self.env.timeout(-(self.mean / self.states) * np.log(1 - np.random.uniform()))
 
 	def run(self):
-		for i in itertools.count():
+		for i in itertools.count(1):
 			for j in itertools.count(self.start_state):
 				if j < self.states:
 					yield self.env.process(self.next_state())
+					self.env.log(i, self.__alias__, "next_state", j, self.states)
 				else:
 					break
-			self.sp.process.interrupt()
+			self.env.log(i, self.__alias__, "reset")
+			self.labile.process.interrupt(i)
+			self.start_state = 0
 
-class SaccadePlanner(object):
-
-	def __init__(self, env, sp, mean=.180):
+class LabileProg(object):
+	__alias__ = "labile_programming"
+	def __init__(self, env, nonlabile, mean=.180):
 		self.env = env
-		self.sp = sp
-		self.mean = mean
-		self.stdev = mean/4.0
-		self.next_saccade = 0
-		self.saccade_id = 0
+		self.nonlabile = nonlabile
+		self.setMean(mean)
+		self.setStdev(mean/4.)
+		self.next_event = 0
 		self.process = env.process(self.run())
+		self.cancellations = 0
 
-	def new_saccade(self):
-		self.next_saccade = np.random.gamma((self.mean*self.mean)/(self.stdev*self.stdev),
-											(self.stdev*self.stdev)/self.mean)
-		self.saccade_id += 1
-		self.env.log(self.saccade_id, "target_selection", "started")
+	def setMean(self, mean):
+		self.mean = mean
+		self.env.log(0, self.__alias__, "set_mean", self.mean)
+
+	def setStdev(self, stdev):
+		self.stdev = stdev
+		self.env.log(0, self.__alias__, "set_stdev", self.stdev)
 
 	def run(self):
 		while True:
-			if self.next_saccade == 0:
-				self.next_saccade = simpy.core.Infinity
-			while self.next_saccade:
+			if self.next_event == 0:
+				self.next_event = simpy.core.Infinity
+			while self.next_event:
 				try:
-					yield self.env.timeout(self.next_saccade)
-					self.next_saccade = 0
+					yield self.env.timeout(self.next_event)
+					self.next_event = 0
 				except simpy.Interrupt as e:
-					if self.next_saccade < simpy.core.Infinity:
-						self.env.log(self.saccade_id, "target_selection", "canceled")
-					self.new_saccade()
-			self.sp.saccade_id = self.saccade_id
-			self.sp.process.interrupt()
-			self.env.log(self.saccade_id, "target_selection", "complete")
+					if self.next_event < simpy.core.Infinity:
+						self.env.log(self.spid, self.__alias__, "canceled")
+						self.cancellations += 1
+					self.spid = e.cause
+					self.next_event = np.random.gamma((self.mean*self.mean)/(self.stdev*self.stdev),
+													  (self.stdev*self.stdev)/self.mean)
+					self.env.log(self.spid, self.__alias__, "started")
+			self.env.log(self.spid, self.__alias__, "complete", self.cancellations)
+			self.cancellations = 0
+			self.nonlabile.process.interrupt(self.spid)
 
-class SaccadeProgrammer(object):
-
+class NonLabileProg(object):
+	__alias__ = "nonlabile_programming"
 	def __init__(self, env, sp, mean=.040):
 		self.env = env
 		self.sp = sp
-		self.mean = mean
-		self.stdev = mean/4.0
-		self.ex_saccade = 0
-		self.saccade_id = 0
+		self.setMean(mean)
+		self.setStdev(mean/4.)
+		self.next_event = 0
 		self.process = env.process(self.run())
 
-	def _ex_saccade(self):
-		self.ex_saccade = np.random.gamma((self.mean*self.mean)/(self.stdev*self.stdev),
-										  (self.stdev*self.stdev)/self.mean)
-		self.env.log(self.saccade_id, "programming", "started")
+	def setMean(self, mean):
+		self.mean = mean
+		self.env.log(0, self.__alias__, "set_mean", self.mean)
+
+	def setStdev(self, stdev):
+		self.stdev = stdev
+		self.env.log(0, self.__alias__, "set_stdev", self.stdev)
 
 	def run(self):
 		while True:
-			if self.ex_saccade == 0:
-				self.ex_saccade = simpy.core.Infinity
-			while self.ex_saccade:
+			if self.next_event == 0:
+				self.next_event = simpy.core.Infinity
+			while self.next_event:
 				try:
-					yield self.env.timeout(self.ex_saccade)
-					self.ex_saccade = 0
+					yield self.env.timeout(self.next_event)
+					self.next_event = 0
 				except simpy.Interrupt as e:
-					if self.ex_saccade < simpy.core.Infinity:
-						raise RuntimeError("programming canceled")
-						self.env.log(self.saccade_id, "programming" "canceled")
-					self._ex_saccade()
-			self.sp.saccade_id = self.saccade_id
-			self.sp.process.interrupt()
-			self.env.log(self.saccade_id, "programming", "complete")
+					self.spid = e.cause
+					if self.next_event < simpy.core.Infinity:
+						raise RuntimeError("nonlabile_programming canceled")
+						self.env.log(self.spid, self.__alias__, "canceled")
+					self.next_event = np.random.gamma((self.mean*self.mean)/(self.stdev*self.stdev),
+													  (self.stdev*self.stdev)/self.mean)
+					self.env.log(self.spid, self.__alias__, "started")
+			self.env.log(self.spid, self.__alias__, "complete")
+			self.sp.process.interrupt(self.spid)
 
 class SaccadeExec(object):
-
-	def __init__(self, env, mean=.040):
+	__alias__ = "saccade_execution"
+	def __init__(self, env, pv, mean=.040):
 		self.env = env
-		self.mean = mean
-		self.stdev = mean/3.0
-		self.ex_saccade = 0
-		self.saccade_id = 0
+		self.pv = pv
+		self.setMean(mean)
+		self.setStdev(mean/3.)
+		self.next_event = 0
 		self.process = env.process(self.run())
+		self.saccades = 0
+		self.mergers = 0
 
-	def _ex_saccade(self):
-		self.ex_saccade = np.random.gamma((self.mean*self.mean)/(self.stdev*self.stdev),
-										  (self.stdev*self.stdev)/self.mean)
-		if self.env.active_saccades == 0:
-			self.env.saccade_id += 1
-		self.env.active_saccades += 1
-		self.env.log(self.saccade_id, "execution", "started")
-		self.env.fixation_durations.append(self.env.now-self.env.fixation_start)
+	def setMean(self, mean):
+		self.mean = mean
+		self.env.log(0, self.__alias__, "set_mean", self.mean)
+
+	def setStdev(self, stdev):
+		self.stdev = stdev
+		self.env.log(0, self.__alias__, "set_stdev", self.stdev)
 
 	def run(self):
 		while True:
-			if self.ex_saccade == 0:
-				self.ex_saccade = simpy.core.Infinity
-			while self.ex_saccade:
+			if self.next_event == 0:
+				self.next_event = simpy.core.Infinity
+			while self.next_event:
 				try:
-					yield self.env.timeout(self.ex_saccade)
-					self.ex_saccade = 0
+					yield self.env.timeout(self.next_event)
+					self.next_event = 0
 				except simpy.Interrupt as e:
-					if self.ex_saccade < simpy.core.Infinity:
-						self.env.log(self.saccade_id, "execution", "merged")
-					self._ex_saccade()
-			self.env.fixation_id += 1
-			self.env.log(self.saccade_id, "execution","complete")
-			self.env.active_saccades -= 1
-			self.env.fixation_start = self.env.now
+					self.spid = e.cause
+					if self.next_event < simpy.core.Infinity:
+						self.env.log(self.spid, self.__alias__, "merged")
+						self.mergers += 1
+					self.next_event = np.random.gamma((self.mean*self.mean)/(self.stdev*self.stdev),
+													  (self.stdev*self.stdev)/self.mean)
+					self.saccades += 1
+					self.pv.process.interrupt((self.spid, True))
+					self.env.log(self.spid, self.__alias__, "started", self.mergers, self.saccades)
+			self.env.log(self.spid, self.__alias__, "complete", self.mergers, self.saccades)
+			self.mergers = 0
+			self.pv.process.interrupt((self.spid, False))
 
-class LoggingEnvironment(simpy.Environment):
+class ProcessVision(object):
 
-	def __init__(self, args, output=sys.__stdout__, initial_time=0):
-		super(LoggingEnvironment, self).__init__(initial_time)
-		self.active_saccades = 0
-		self.saccade_id = 0
-		self.cancellations = 0
-		self.fixation_id = 1
-		self.fixation_start = 0
-		self.fixation_durations = []
-		if output!=sys.__stdout__:
-			self.out = open(os.path.abspath(output))
+	def __init__(self, env):
+		self.env = env
+		self.process = env.process(self.run())
+		self.fixations = 1
+
+	def run(self):
+		self.env.log(1, "fixation", "started")
+		while True:
+			try:
+				yield self.env.timeout(simpy.core.Infinity)
+			except simpy.Interrupt as e:
+				if e.cause[1]:
+					self.env.log(-1, "fixation", "complete", self.fixations)
+				else:
+					self.fixations += 1
+					self.env.log(-1, "fixation", "started", self.fixations)
+
+class CRISPEnvironment(simpy.Environment):
+
+	def __init__(self, args, initial_time=0.0):
+		super(CRISPEnvironment, self).__init__(initial_time)
+		self.stop = -1
+
+	def log(self, *args):
+		if self.stop==-1:
+			e = [-1] + list(args)
 		else:
-			self.out = output
-		self.out.write("# %s\n" % args)
-		self.out.write("time\tactive_saccades\tcancellations\tsaccade_id\tfixation_id\ttimer_id\tstage\tstatus\n")
+			e = [self.now] + list(args)
+			if self.efun(e):
+				self.stop = True
+		print e
 
-	def log(self, id, stage, status):
-		sac_id = self.saccade_id if self.active_saccades>0 else 0
-		fix_id = self.fixation_id if self.active_saccades==0 else 0
-		if stage=="execution":
-			fix_id = self.fixation_id
-		elif stage=="target_selection":
-			if status=="canceled":
-				self.cancellations += 1
-			elif status=="complete":
-				self.cancellations = 0
-		self.out.write("%f\t%d\t%d\t%d\t%d\t%d\t%s\t%s\n" % (self.now, self.active_saccades, self.cancellations, sac_id, fix_id, id, stage, status))
-		self.out.flush()
+	def run_while(self, efun):
+		self.efun = efun
+		self.stop = 0
+		while self.stop==0:
+			self.step()
+
 
 if __name__ == '__main__':
 
@@ -170,7 +214,7 @@ if __name__ == '__main__':
 						help="the average timer interval in ms")
 	parser.add_argument("--timer_states", type=int, default=11,
 						help="the number of discrete states in the random walk timer")
-	parser.add_argument("--timer_start_state", type=int, default=0,
+	parser.add_argument("--timer_start_state", type=int, default=-1,
 						help="the starting state of the random walk timer")
 	parser.add_argument("--labile_mean", type=float, action="store", default=.180,
 						help="the average timer interval in ms")
@@ -180,16 +224,14 @@ if __name__ == '__main__':
 						help="the average timer interval in ms")
 	args = vars(parser.parse_args())
 
-	env = LoggingEnvironment(args)
+	env = CRISPEnvironment(args)
 
 	# Create components
-	saccade_exec = SaccadeExec(env, mean=args['exec_mean'])
-	saccade_programmer = SaccadeProgrammer(env, saccade_exec, mean=args['nonlabile_mean'])
-	saccade_planner = SaccadePlanner(env, saccade_programmer, mean=args['labile_mean'])
-	brainstem_oscillator = BrainstemOscillator(env, saccade_planner, mean=args['timer_mean'],
-											   states=args['timer_states'],
-											   start_state=args['timer_start_state'])
+	processVision = ProcessVision(env)
+	saccadeExec = SaccadeExec(env, processVision, mean=args['exec_mean'])
+	nonLabileProg = NonLabileProg(env, saccadeExec, mean=args['nonlabile_mean'])
+	labileProg = LabileProg(env, nonLabileProg, mean=args['labile_mean'])
+	timer = Timer(env, labileProg, mean=args['timer_mean'], states=args['timer_states'], start_state=args['timer_start_state'])
 
 	# Run
-	while env.saccade_id < args['max_saccades']:
-		env.step()
+	env.run_while(lambda e: e[2]=="saccade_execution" and e[3]=="complete" and e[5]==args["max_saccades"])
