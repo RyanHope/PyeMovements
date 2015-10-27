7a338c32da3e5a50e107c64a609166f9e1b3d14e
Ryan Hope <rmh3093@gmail.com>
2015-10-27 12:23:05 -0400

    defaul max trials 2500

--- antisaccade.py
+++ antisaccade.py
@@ -104,7 +104,7 @@ def get_args(args=sys.argv[1:]):
 	import argparse
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--max-trials", type=int, default=1)
+	parser.add_argument("--max-trials", type=int, default=2500)
 	parser.add_argument("--timer_mean1", type=float, action="store", default=0.250)
 	parser.add_argument("--timer_mean2", type=float, action="store", default=0.250)
 	parser.add_argument("--timer_mean3", type=float, action="store", default=0.250)
