f6b9f6861c53cc1f1cfc89d95fe3b80698dfe87e
Hope <hoper2@cwl-projects.cogsci.rpi.edu>
2015-11-13 10:50:23 -0500

    idk

--- mm.R
+++ mm.R
@@ -5,11 +5,40 @@ require(rhdf5)
 require(ggplot2)
 require(parallel)
 require(data.table)
+require(reshape2)
 
-d = h5read("results.h5","points",compoundAsDataFrame=FALSE)
+d = h5read("~/PyeMovements/results.h5","points",compoundAsDataFrame=FALSE)
 
-dd = rbindlist(mclapply(1:length(d$latencies$labile_mean), function(x, z) {
-  .d = data.table(density=density(z$latencies[,x],n=200,from=0,to=2)$y)
-  .d[,point:=x]
-  .d
-}, d$latencies, mc.co))
+all_summary = fread(tail(list.files("/CogWorks/Projects/SaccadeTask/",pattern="^summary_|\\.dat$",full.names=T),1))
+all_summary[,correct:=ifelse(saccade.response.target.correct,
+                             paste(mode,"correct",sep="-"),
+                             paste(mode,"incorrect",sep="-"))]
+all_summary_clean = all_summary[average.quality==1 & !is.na(first.saccade.latency)]
+
+dd = rbindlist(mclapply(1:length(d$latencies$labile_mean), function(x, z, zz) {
+  .ks = ks.test(zz[sid=="0304b"&correct=="pro-correct",first.saccade.latency]*1000,z$latencies[,x])
+  data.table(
+    id=x,
+    timer_states=z$timer_states[x],
+    timer_mean1=z$timer_mean1[x],
+    timer_mean2=z$timer_mean2[x],
+    timer_mean3=z$timer_mean3[x],
+    labile_mean=z$labile_mean[x],
+    labile_stdev=z$labile_stdev[x],
+    cue_cancel_prob=z$cue_cancel_prob[x],
+    ks.stat=.ks$statistic,
+    ks.p=.ks$p.value
+  )
+}, d$latencies, all_summary_clean, mc.cores=12))
+dd[order(ks.stat),]
+
+#data=density(all_summary_clean[sid="e35da"&correct=="pro-correct",first.saccade.latency]*1000,from=0,to=1000)
+id = 421937
+.den = data.table(
+  latency = c(all_summary_clean[sid=="0304b"&correct=="pro-correct",first.saccade.latency]*1000,
+              sample(d$latencies$latencies[,id],150)),
+  model = c(rep("data",all_summary_clean[sid=="0304b"&correct=="pro-correct",.N]),rep("model",150))
+)
+
+ggplot(melt(.den,id.var="freq",value.name="density")) + 
+  geom_line(aes(x=freq,y=density,color=variable,group=variable))
--- process_mm.py
+++ process_mm.py
@@ -4,34 +4,48 @@ import sys, os
 from tables import *
 
 class CRISP(IsDescription):
-    timer_mean  = Float32Col()
+    timer_states = UInt16Col()
+    timer_mean1  = Float32Col()
+    timer_mean2  = Float32Col()
+    timer_mean3  = Float32Col()
     labile_mean  = Float32Col()
-    gap_cancel_prob  = Float32Col()
+    labile_stdev = Float32Col()
     cue_cancel_prob  = Float32Col()
-    latencies  = UInt16Col(5000)
+    latencies  = UInt16Col(2500)
 
 if __name__ == '__main__':
+	
     if len(sys.argv) != 3:
-        print "Usage: process_mm.py [RESULT_FILE] [JOB_NAME]"
+        print "Usage: process_mm.py [RESULT_FILE] [JOB_sNAME]"
         sys.exit(-1)
+    
     fin = open(sys.argv[1], "r")
     base = os.path.splitext(sys.argv[1])[0]
-    header = fin.readline().strip().split("\t")#[:-2] + ["latency"]
+    header = fin.readline().strip().split("\t")
+    
     h5file = open_file("%s.h5" % base, mode = "w", title = sys.argv[2])
     points = h5file.create_group("/", 'points', 'FullMeshSpace')
     table = h5file.create_table(points, 'latencies', CRISP, "Antisacade First Saccade Latencies with Gap & Cue Cancellation")
     crisp = table.row
     for line in fin:
         line = line.strip().split("\t")[:-1]
-        if len(line) == 6:
-            crisp["timer_mean"] = line[1]
-            crisp["labile_mean"] = line[2]
-            crisp["gap_cancel_prob"] = line[3]
-            crisp["cue_cancel_prob"] = line[4]
-            crisp["latencies"] = line[-1].split("|")
-            crisp.append()
+        if len(line) == 8:
+			latencies = line[-1].split("|")
+			if len(latencies) == 2500:
+				crisp["timer_states"] = line[0]
+				crisp["timer_mean1"] = line[1]
+				crisp["timer_mean2"] = line[2]
+				crisp["timer_mean3"] = line[3]
+				crisp["labile_mean"] = line[4]
+				crisp["labile_stdev"] = line[5]
+				crisp["cue_cancel_prob"] = line[6]
+				crisp["latencies"] = latencies
+				crisp.append()
+			else:
+				print line[0:7]
         else:
             print line
+    
     table.flush()
     h5file.close()
     fin.close()
