d59f356416f6971109f69ac9b8637903cfeac6be
Ryan Hope <rmh3093@gmail.com>
2015-11-24 12:22:12 -0500

    asdas

--- score_models.py
+++ score_models.py
@@ -6,25 +6,23 @@ from scipy.stats import ks_2samp
 
 if __name__ == '__main__':
 
-    if len(sys.argv) != 2:
-        print "Usage: process_mm.py [RESULT_FILE]"
+    if len(sys.argv) != 3:
+        print "Usage: process_mm.py [RESULT_FILE] [SUBJECT]"
         sys.exit(-1)
 
-    with open("scores.txt","w") as scores:
-        with open(sys.argv[1], "r") as fin:
-            header = fin.readline().strip().split("\t")
-            with open("latencies.csv","r") as data:
-                subjects = {}
-                for line in data.readlines():
-                    line = line.strip().split(",")
-                    lat = [float(l) for l in line[1:]]
-                    subjects[line[0]] = lat
-                with open("scores.csv","w") as o:
-                    for line in fin:
-                        line = line.strip().split("\t")[:-1]
-                        if len(line) == 8:
-                            latencies = [float(l)/1000 for l in line[-1].split("|")]
-                            for s in subjects.keys():
-                                d,p = ks_2samp(subjects[s],latencies)
-                                scores.write(s+"\t"+"\t".join(line[:-1]) + "\t%.3f\t%.3f" % (d,p) + "\n")
-                                scores.flush()
+    with open(sys.argv[1], "r") as fin:
+        header = fin.readline().strip().split("\t")
+        with open("latencies.csv","r") as data:
+            subjects = {}
+            for line in data.readlines():
+                line = line.strip().split(",")
+                lat = [float(l) for l in line[1:]]
+                subjects[line[0]] = lat
+            s = subjects.keys()[int(sys.argv[2])]
+            with open("scores_%s.txt" % s, "w") as scores:
+                for line in fin:
+                    line = line.strip().split("\t")[:-1]
+                    latencies = [float(l)/1000 for l in line[-1].split("|")]
+                    d,p = ks_2samp(subjects[s],latencies)
+                    scores.write(s+"\t"+"\t".join(line[:-1]) + "\t%.3f\t%.3f" % (d,p) + "\n")
+                    scores.flush()
