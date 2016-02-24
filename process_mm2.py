7a5c72c41e1a17dc56b194631e752ee4edf2b52f
Hope <hoper2@cwl-projects.cogsci.rpi.edu>
2016-02-23 23:40:12 -0500

    update process script

--- process_mm2.py
+++ process_mm2.py
@@ -5,7 +5,7 @@ from tables import *
 
 class CRISP(IsDescription):
     sid = StringCol(5)
-    timer_states = UInt16Col()
+    timer_states = Float32Col()
     timer_mean  = Float32Col()
     labile_mean  = Float32Col()
     labile_stdev = Float32Col()
@@ -16,8 +16,12 @@ class CRISP(IsDescription):
     cue_timer_rate  = Float32Col()
     gap_timer_rate  = Float32Col()
     alpha  = Float32Col()
-    lat_mean = Float32Col()
-    lat_std = Float32Col()
+    lat_mean_pro = Float32Col()
+    amp_mean_pro = Float32Col()
+    rsp_mean_pro = Float32Col()
+    lat_mean_anti = Float32Col()
+    amp_mean_anti = Float32Col()
+    rsp_mean_anti = Float32Col()
 
 
 if __name__ == '__main__':
@@ -29,18 +33,15 @@ if __name__ == '__main__':
     sids = ["0304b","06fbf","1f330","31fc9","3d5bd","427fc","49c3b","547cb",
             "57548","6de95","7a44c","8b747","a314d","a9e6e","b422c","b4a6f",
             "b608f","bd801","c39a8","d17cc","d4912","e1070","e35da"]
-    stat = ["mean","std"]
-    measure = "lat"
-    mode = "pro"
 
     fin = open(sys.argv[1], "r")
     base = os.path.splitext(sys.argv[1])[0]
     header = fin.readline().strip().split("\t")
     N = len(header)
 
-    h5file = open_file("%s_%s_%s.h5" % (base,mode,measure), mode = "w", title = sys.argv[2])
-    points = h5file.create_group("/", 'points', 'FullMeshSpace')
-    table = h5file.create_table(points, 'latencies', CRISP, "Antisacade First Saccade Latencies with Gap & Cue Cancellation")
+    h5file = open_file("%s.h5" % (base), mode = "w", title = sys.argv[2])
+    #points = h5file.create_group("/", 'points', 'FullMeshSpace')
+    table = h5file.create_table("/", 'mmfits', CRISP, "CRISP MindModeling Fits")
 
     crisp = table.row
     for line in fin:
@@ -49,21 +50,16 @@ if __name__ == '__main__':
         for c in range(11,N-1):
             data[header[c]] = line[c]
         for sid in sids:
-            crisp["sid"] = sid
-            crisp["timer_states"] = line[0]
-            crisp["timer_mean"] = line[1]
-            crisp["labile_mean"] = line[2]
-            crisp["labile_stdev"] = line[3]
-            crisp["attn_mean"] = line[4]
-            crisp["attn_stdev"] = line[5]
-            crisp["gap_cancel_prob"] = line[6]
-            crisp["gap_timer_rate"] = line[7]
-            crisp["cue_cancel_prob"] = line[8]
-            crisp["cue_timer_rate"] = line[9]
-            crisp["alpha"] = line[10]
-            crisp["lat_mean"] = data["pro_lat_mean_%s" % sid]
-            crisp["lat_std"] = data["pro_lat_std_%s" % sid]
-            crisp.append()
+          crisp["sid"] = sid
+          for i in xrange(11):
+            crisp[header[i]] = line[i]
+          crisp["lat_mean_pro"] = data["pro_lat_mean_%s" % sid]
+          crisp["lat_mean_anti"] = data["anti_lat_mean_%s" % sid]
+          crisp["amp_mean_pro"] = data["pro_amp_mean_%s" % sid]
+          crisp["amp_mean_anti"] = data["anti_amp_mean_%s" % sid]
+          #crisp["rsp_mean_pro"] = data["pro_rsp_mean_%s" % sid]
+          #crisp["rsp_mean_anti"] = data["anti_rsp_mean_%s" % sid]
+          crisp.append()
 
     table.flush()
     h5file.close()
